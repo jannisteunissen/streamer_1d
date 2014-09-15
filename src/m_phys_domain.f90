2b291ba647fa8d8a02aabbe97febd4f9d0e96a0a
Jannis Teunissen <jannis@teunissen.net>
2014-09-15 17:39:11 +0200

    use protected to create simple modules with data

--- src/m_phys_domain.f90
+++ src/m_phys_domain.f90
@@ -7,18 +7,18 @@ module m_phys_domain
 
   real(dp), protected         :: PD_length
   real(dp), protected         :: PD_dx, PD_inv_dx
-  integer                     :: PD_grid_size
+  integer, protected          :: PD_grid_size
 
 contains
 
   subroutine PD_set(dx, grid_size)
     real(dp), intent(in) :: dx
-    integer, intent(in) :: grid_size
+    integer, intent(in)  :: grid_size
 
-    PD_dx = dx
-    PD_inv_dx = 1/dx
+    PD_dx        = dx
+    PD_inv_dx    = 1/dx
     PD_grid_size = grid_size
-    PD_length = dx * (grid_size-1)
+    PD_length    = dx * (grid_size-1)
   end subroutine PD_set
 
 end module m_phys_domain
